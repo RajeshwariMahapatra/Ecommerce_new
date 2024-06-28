21b16d75ff577fb9b6a202fa0e50921b9d1fe607
DevVora15 <voradev3@gmail.com>
2024-06-28 17:46:23 +0530

    admin.php created

---
+++ fucntions.php
@@ -0,0 +1,33 @@
+<?php
+// require("config.php");
+
+// Generate Alphanumeric String for all the tables
+function generateRandomString($length = 12) {
+    $alphanumeric = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $alphanumericLength = strlen($alphanumeric);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $alphanumeric[random_int(0, $alphanumericLength - 1)];
+    }
+    return $randomString;
+}
+function isUnique($string,$tableName,$columnName) {
+    $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM $tableName WHERE $columnName = :value");
+    $stmt->bindParam(':value',$string);
+    $stmt->execute();
+    return $stmt->fetchColumn() == 0;
+}
+function uniqueRandomString($length = 12,$tableName,$columnName) {
+    do {
+        $randomstring = generateRandomString($length);
+    } while (!isUnique($randomstring,$tableName,$columnName));
+
+    return $randomstring;
+}
+
+
+// $something = uniqueRandomString(12,'Users','user_identity');
+// echo $something;
+
+?>
