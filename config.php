3488bcf6c16b212bad8f175959a3d36a9d50c199
DevVora15 <voradev3@gmail.com>
2024-07-01 14:15:52 +0530

    my config.php file

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+ini_set( "display_errors", true );
+date_default_timezone_set( "Asia/Kolkata" );
+define( "DB_DSN", "mysql:host=localhost;dbname=ecommerce_new_db" );
+define( "DB_USERNAME", "root" );
+define( "DB_PASSWORD", "" );
+define( "CLASS_PATH", "classes" );
+define( "TEMPLATE_PATH", "templates" );
+define( "ADMIN_USERNAME", "admin" );
+define( "ADMIN_PASSWORD", "mypass" );
+define( "PRODUCT_IMAGE_PATH", "images/products" );
+define( "PRODUCT_IMAGE_URL", "images/products" );
+
+require( CLASS_PATH . "/ProductCategory.php" );
+require( CLASS_PATH . "/Product.php" );
+require( CLASS_PATH . "/Brand.php" );
+
+function handleException( $exception ) {
+    echo "Sorry, a problem occurred. Please try later.";
+    error_log( $exception->getMessage() );
+  }
+  set_exception_handler( 'handleException' );
+
+?>
